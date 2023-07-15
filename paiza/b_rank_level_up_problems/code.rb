1b82094477fc03634f39629a2cd13c4ebe1a91d4
Hiroshi Okimura <hiroshi1279yku96okmr@gmail.com>
2023-07-15 23:03:02 +0900

    paiza: 20230715

--- paiza/b_rank_level_up_problems/code.rb
+++ paiza/b_rank_level_up_problems/code.rb
@@ -124,3 +124,34 @@ hash = hash.sort_by { |_, v| -v }.to_h
 hash.each do |key, value|
     puts "#{key} #{value}"
 end
+
+
+5行5列の五目並べの盤面が与えられます。
+
+盤面の各マスには、"O"か"X"か"."が書かれています。
+
+"O"と"X"は、それぞれプレイヤーの記号を表します。
+
+同じ記号が横に連続で5つ並んでいれば、その記号のプレイヤーが勝者となります。
+
+勝者の記号を1行で表示してください。
+勝者がいない場合は、引き分けとして、"D"を表示してください。
+
+arr = %w[O X]
+result = 'D'
+
+5.times do
+    str = gets.chomp.split('')
+    count = 0
+    arr.each do |a|
+        str.each do |s|
+            count += 1 if a == s
+            if count == 5
+                result = a
+                break
+            end
+        end
+    end
+end
+
+puts result
