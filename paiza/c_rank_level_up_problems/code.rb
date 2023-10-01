3a03d508bc72c0127a0d1027270892ea41e12fea
Hiroshi Okimura <hiroshi1279yku96okmr@gmail.com>
2023-10-01 15:02:20 +0900

    paiza_c

--- paiza/c_rank_level_up_problems/code.rb
+++ paiza/c_rank_level_up_problems/code.rb
@@ -794,3 +794,18 @@ m.times do
 
     puts "#{n} #{point}"
 end
+
+
+
+C021
+
+xc, yc, r_1, r_2 = gets.split.map(&:to_i)
+n = gets.to_i
+n.times do
+    x, y = gets.split.map(&:to_i)
+    if r_1 ** 2 <= ((x - xc) ** 2) + ((y - yc) ** 2) && r_2 ** 2 >= ((x - xc) ** 2) + ((y - yc) ** 2)
+        puts "yes"
+    else
+        puts "no"
+    end
+end
